61d9757a244254eb1ec9ebf4b20df375ffa89ac8
Youkamii <gkfkd747@naver.com>
2025-03-03 18:14:57 +0900

    feat(hub): add geometry point 4236 to hub #9

--- src/main/java/com/logistics/moolryu/domains/hub/entity/Hub.java
+++ src/main/java/com/logistics/moolryu/domains/hub/entity/Hub.java
@@ -1,5 +1,8 @@
 package com.logistics.moolryu.domains.hub.entity;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.locationtech.jts.geom.Point;
 import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -19,4 +22,8 @@ public class Hub {
 
     @Column(nullable = false)
     private String hubAddress;
+
+    @JdbcTypeCode(SqlTypes.GEOMETRY)
+    @Column(columnDefinition = "geometry(Point,4326)", nullable = false)
+    private Point location;
 }
